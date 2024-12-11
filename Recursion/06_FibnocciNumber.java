bff25090281775ddfaa0878948f49286b52496f9
Sandeep <sandeep7668111757@gmail.com>
2024-12-11 07:10:52 +0000

    number of digit

--- Recursion/06_FibnocciNumber.java
+++ Recursion/06_FibnocciNumber.java
@@ -5,9 +5,9 @@ public class Main {
 
 	public static int fib(int n ){
         // write code here
-       if(n==1){
+       if(n==0){
         return 0;
-       }else if(n==2){
+       }else if(n==1){
         return 1;
        }
        int fPrev = fib(n-1);
