9b7f38666e9fc8d113048f5f6275c8861ac78fba
Sandeep <sandeep7668111757@gmail.com>
2024-12-13 21:32:55 +0530

    Create 22_MazePath.java
    Maze Path

---
+++ Recursion/22_MazePath.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main { 
+   // store paths in ans array
+
+    static ArrayList<String> ans = new ArrayList<>();
+    static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+            
+             if(dr<sr || dc<sc){
+              return;
+             }
+             if(dr == sr && dc == sc){
+              ans.add(psf);
+              return;
+             }
+             
+            for(int i=1; sc + i <= dc; i++){
+                 printMazePaths(sr, sc +i,dr, dc,psf+"h"+i);
+            }
+            for(int i=1; sr + i <= dr; i++){
+               printMazePaths( sr+i, sc,dr, dc, psf+"v"+i);
+            }
+            for(int i=1; sc + i<= dc && sr + i <= dr; i++){
+                printMazePaths(sr +i, sc +i,dr, dc,psf+"d"+i);
+            }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        printMazePaths(0, 0, n - 1, m - 1, "");
+        Collections.sort(ans);
+
+        for(String path : ans){
+            System.out.println(path);
+        }
+    }
+}
