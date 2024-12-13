bbdaa60069b7a4d704672d15e37b8f35aa48db54
Sandeep <sandeep7668111757@gmail.com>
2024-12-13 11:45:40 +0000

    Celsius to Fahrenheit

---
+++ 01_basicOf-Java/03_Celsius to Fahrenheit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner scr = new Scanner(System.in);
+    int tempInCel = scr.nextInt();
+    int  fahrenheit = (tempInCel * 9/5) +32;
+    System.out.println(fahrenheit);
+
+    }
+}
