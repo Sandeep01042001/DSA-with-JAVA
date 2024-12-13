57f3cec001164725ed8013f39f806f080073c3b2
Sandeep <sandeep7668111757@gmail.com>
2024-12-13 11:43:37 +0000

    N star

---
+++ 01_basicOf-Java/02_printNStar.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int n=sc.nextInt();
+        for(int i=0; i<n; i++)
+        System.out.print("*" + " ");
+
+        System.out.println();
+        for(int i=0; i<n; i++)
+        System.out.println("*");
+       
+    }
+}
