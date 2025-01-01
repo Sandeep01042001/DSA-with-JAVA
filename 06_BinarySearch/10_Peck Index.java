a8553802f2095db2e4a0b2c5a2f7a8e071fbde5c
Sandeep <sandeep7668111757@gmail.com>
2025-01-01 19:37:28 +0530

    Peck Index

---
+++ 06_BinarySearch/10_Peck Index.java
@@ -0,0 +1,63 @@
+import java.util.*;
+/* https://course.acciojob.com/idle?question=e6a1d307-59de-4d83-b174-943c69c5b042 */
+class Solution{
+    public int findPeak(int arr[], int n)
+    {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if ((mid == 0 || arr[mid - 1] <= arr[mid]) && (mid == n - 1 || arr[mid + 1] <= arr[mid])) {
+                return mid;
+            } else if (mid > 0 && arr[mid - 1] > arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+       
+    }
+}
+
+class Main{
+	public static void main(String[] args){
+		try (Scanner sc = new Scanner(System.in)) {
+			int t=sc.nextInt();
+			while(t>0){
+				int n=sc.nextInt();
+				int []A=new int[n];
+				for(int i=0;i<n;i++){
+					A[i]=sc.nextInt();
+				}
+				Solution ob =new Solution();
+				int ans = ob.findPeak(A,n);
+                if(ans==0){
+                    if(A[ans]>A[ans+1]){
+                        System.out.println("true");
+                    }
+                    else{
+                       System.out.println("false");    
+                    }
+                }
+                else if(ans==n-1){
+                    if(A[ans]>A[ans-1]){
+                        System.out.println("true");                        
+                    }
+                    else{
+                        System.out.println("false");                        
+                    }
+                }
+                else{
+                    if(A[ans]>A[ans+1] && A[ans]>A[ans-1]){
+                        System.out.println("true");
+                    }
+                    else{
+                        System.out.println("false");                        
+                    }
+                }
+				t--;
+			}
+		}
+	}
+}
