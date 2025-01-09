cc06a6d7f0f38a1d213271c561f0b3ec4f211a95
Sandeep <sandeep7668111757@gmail.com>
2025-01-09 19:42:31 +0530

    Special Sum of Array

---
+++ 01_basicOf-Java/20_Special Sum of Array.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+/* https://course.acciojob.com/idle?question=e5595611-2bcb-459f-9ef2-dc305341928c */
+class Solution {
+    static int specialSum(ArrayList<Integer> arr, int n) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        while (sum >= 10) {
+            int tempSum = 0;
+            while (sum > 0) {
+                tempSum += sum % 10;
+                sum /= 10;
+            }
+            sum = tempSum;
+        }
+
+        return sum;
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+        int ans = Solution.specialSum(arr, n);
+        System.out.println(ans);
+    }
+}
