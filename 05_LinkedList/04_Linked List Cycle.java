95cda676fe0ab88602ff8029bf543d6dc51cec98
Sandeep <sandeep7668111757@gmail.com>
2025-01-11 21:22:50 +0530

    Linked List Cycle

---
+++ 05_LinkedList/04_Linked List Cycle.java
@@ -0,0 +1,111 @@
+import java.util.*;   /* https://course.acciojob.com/idle?question=130e273c-3811-43a0-95e0-2862dbd39118 */
+import java.io.*;
+import java.lang.*;
+
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+
+class LinkedList
+{
+    Node head;  
+    Node tail;
+    public void addToTheLast(Node node) 
+    {
+      if (head == null) 
+      {
+       head = node;
+       tail = node;
+      } 
+      else 
+      {
+       tail.next = node;
+       tail = node;
+      }
+    }
+    
+    public static void makeLoop(Node head, int x){
+        if (x == 0)
+            return;
+        Node curr = head;
+        Node last = head;
+
+        int currentPosition = 1;
+        while (currentPosition < x)
+        {
+            curr = curr.next;
+            currentPosition++;
+        }
+        
+        while (last.next != null)
+            last = last.next;
+        last.next = curr;
+    }
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+    
+}
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        LinkedList llist= new LinkedList(); 
+        int a1=sc.nextInt();
+        Node head= new Node(a1);
+        llist.addToTheLast(head);
+        for (int i = 1; i < n; i++) 
+        {
+            int a = sc.nextInt(); 
+            llist.addToTheLast(new Node(a));
+        }
+            
+            
+        int pos = sc.nextInt();
+        llist.makeLoop(head, pos);
+            
+        Solution x = new Solution();
+        boolean a=x.detectLoop(head);
+        
+        if(a==true)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        
+    }
+}
+
+class Solution
+{
+    public static boolean detectLoop(Node head){
+    //your code here
+        if (head == null || head.next == null) {
+            return false; // No cycle possible if the list is empty or has only one node
+        }
+
+        Node slow = head; // Slow pointer (Tortoise)
+        Node fast = head; // Fast pointer (Hare)
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;         // Move slow pointer one step
+            fast = fast.next.next;    // Move fast pointer two steps
+
+            if (slow == fast) {
+                return true; // Cycle detected
+            }
+        }
+
+        return false; 
+    }
+}
