8c132a6db9d4f1ed751dbce1033378c8cd2ec878
Sandeep <sandeep7668111757@gmail.com>
2025-04-02 13:02:52 +0530

    Top View Of A Binary Tree

---
+++ 11_BinaryTree/08_TopViewOfABinaryTree.java
@@ -0,0 +1,124 @@
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class Main {
+    static Node buildTree(String str){
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if(i >= ip.length)
+                break;   
+            currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+			String s = br.readLine();
+			Node root = buildTree(s);
+			Solution tree = new Solution();
+			ArrayList<Integer> arr = tree.topView(root);
+			for(int x : arr)
+			System.out.print(x +" ");
+			System.out.println();  
+		}
+	}
+}
+  
+
+class qObj
+{
+    Node node;
+    int level;
+    qObj(Node n,int l)
+    {
+        node=n;
+        level=l;
+    }
+}
+
+class Solution
+{
+    public class Pair{
+        Node node;
+        int col;
+        Pair(Node node, int col){
+            this.node = node;
+            this.col = col;
+        }
+    }
+    public ArrayList<Integer> topView(Node root){
+        if(root == null) return new ArrayList<>();
+        LinkedList<Pair> q = new LinkedList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        q.addLast(new Pair(root, 0));
+        int level = 1;
+        while(q.size()>0){
+            int size = q.size();
+            while(size-- != 0){
+                Pair rem = q.removeFirst();
+                int col = rem.col;
+                if(!map.containsKey(col)){
+                   map.put(col, rem.node.data);
+                }
+                if(rem.node.left != null){
+                    q.addLast(new Pair(rem.node.left, col-1));
+                }
+                 if(rem.node.right != null){
+                    q.addLast(new Pair(rem.node.right, col+1));
+                }
+            }
+            level++;
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int key : map.keySet()){
+            ans.add(map.get(key));
+        }
+        return ans;
+    }
+}
+
