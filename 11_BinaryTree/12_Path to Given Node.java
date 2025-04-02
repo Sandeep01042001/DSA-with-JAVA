1e06cfe3f0af22af9569f7f989943c955d8442c4
Sandeep <sandeep7668111757@gmail.com>
2025-04-02 16:02:02 +0530

    Path to Given Node

---
+++ 11_BinaryTree/12_Path to Given Node.java
@@ -0,0 +1,95 @@
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class Main {
+    static Node buildTree(String str){
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if(i >= ip.length)
+                break;   
+            currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+    
+	public static void main (String[] args) throws IOException{
+            Scanner sc = new Scanner(System.in);
+            int n=sc.nextInt();
+            sc.nextLine();
+			String s = sc.nextLine();
+            int b=sc.nextInt();
+			Node root = buildTree(s);
+			Solution tree = new Solution();
+			ArrayList<Integer> ans=tree.solve(root,b);
+            for(int i=0;i<ans.size();i++)
+            System.out.print(ans.get(i)+" ");
+
+            System.out.println();
+
+            sc.close();			
+	}
+}
+  
+
+
+class Solution{
+    
+   public ArrayList<Integer> solve(Node root,int b)
+	{
+        if(root == null){
+            return new ArrayList<>();
+        }
+        if(root.data == b){
+            ArrayList<Integer> li = new ArrayList<>();
+            li.add(root.data);
+            return li;
+        }
+
+        ArrayList<Integer> left = solve(root.left, b);
+        if(left.size() != 0){
+            left.add(0, root.data);
+            return left;
+        }
+        ArrayList<Integer> right = solve(root.right, b);
+        if(right.size() != 0){
+            right.add(0, root.data);
+            return right;
+        }
+        return new ArrayList<>(); 
+    }
+}
