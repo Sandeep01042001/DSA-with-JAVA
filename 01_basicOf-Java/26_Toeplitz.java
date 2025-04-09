77d967be9485c092b5fdb65a51a02729a429ad4f
Sandeep <sandeep7668111757@gmail.com>
2025-04-09 16:37:10 +0530

    Toeplitz matrix

---
+++ 01_basicOf-Java/26_Toeplitz.java
@@ -0,0 +1,17 @@
+/* Input: matrix = [[1,2,3,4],
+                    [5,1,2,3], 
+                    [9,5,1,2] 
+                             ]
+Output: true*/
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i=0; i<matrix.length-1; i++){
+            for(int j=0; j<matrix[0].length-1; j++){
+                if(matrix[i][j] != matrix[i+1][j+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
