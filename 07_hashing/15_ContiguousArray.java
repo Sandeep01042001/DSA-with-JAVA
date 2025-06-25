1f2b8ff6c2b162cf2823fcb0e45635bccc961ef4
Sandeep <sandeep7668111757@gmail.com>
2025-06-25 16:32:23 +0530

    Contiguous Array

--- 07_hashing/15_ContiguousArray.java
+++ 07_hashing/15_ContiguousArray.java
@@ -1,4 +1,5 @@
 /*https://leetcode.com/problems/contiguous-array/description/*/
+/*Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.*/
 class Solution {
     public int findMaxLength(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
